bb833316d52da18ee0d3833c0a35efcb9ebe477c
fators22 <116498723+fators22@users.noreply.github.com>
2022-12-13 08:41:04 -0500

    Create SportsTeam.java

---
+++ src/main/java/SportsTeam.java
@@ -0,0 +1,31 @@
+public class SportsTeam {
+
+/*Create  5 instance variables :a String representing the Team’s name, an int representing the number of games played, 
+an int representing the games won and an int representing the games lost and an int representing the number of ties.*/
+ 
+  
+  
+  
+  
+/*Write two different constructors for this class.  The first constructor should have a single parameter representing 
+the team name and all other instance variables should be initialized to zero.  The second constructor should have 5 parameters
+representing a value to initialize each of the instance variables to.*/
+  
+  
+  
+  
+
+// Write a method called getWinningPercentage().  This method should return an int representing the percentage of games won.
+
+// Write five separate "getters" that allow access to the class instance variables.
+
+/*Write a void method called playGame() that takes a String as a parameter.  
+  A "W" should increment the games won, an "L" should increment the games lost and any other String
+    should increment the games tied*/
+
+//Write a toString() method that returns a String with all of the stats for a team.
+  
+  
+  
+  
+}
